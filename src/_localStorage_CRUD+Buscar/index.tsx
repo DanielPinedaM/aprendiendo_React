81d7d829391c1fe72dc305d995e77cc2ba6f45d6
daniel <daniel.pinedamo@amigo.edu.co>
2024-01-24 23:44:39 -0500

    localStorage

---
+++ src/_localStorage_CRUD+Buscar/index.tsx
@@ -0,0 +1,10 @@
+const MyFunctionalComponent = () => {
+  return (
+    <div>
+      <h1>CRUD + Buscar usando local storage</h1>
+      <h2>INCOMPLETO</h2>
+    </div>
+  );
+};
+
+export default MyFunctionalComponent;
