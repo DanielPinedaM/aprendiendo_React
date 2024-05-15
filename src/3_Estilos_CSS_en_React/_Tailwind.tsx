613091abd4a0a292f984b2fc16ce0813f061136b
daniel <daniel.pinedamo@amigo.edu.co>
2024-05-14 22:13:59 -0500

    clsx y tailwind-merge

--- src/3_Estilos_CSS_en_React/_Tailwind.tsx
+++ src/3_Estilos_CSS_en_React/_Tailwind.tsx
@@ -1,4 +1,12 @@
 /*
-Fazt - Configurar React + Tailwind
+## Fazt
+
+* Configurar React + Tailwind
 https://www.youtube.com/watch?v=rLoWMU4L_qE&t=16204s
+
+* React y Tailwind - Crea componentes reutilizables y evita repetir codigo
+https://youtu.be/GAcq61I9IVM?si=cZrpmHGz8G4cHl1g
+
+* clsx y tailwind-merge
+https://youtu.be/cJsRaYmrSQM?si=_NSyAOmLfm-uta91
 */
